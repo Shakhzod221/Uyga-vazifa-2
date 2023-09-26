cffc2a4e012df53f4db945cf762e864d11431797
Shakhzod221 <130886474+Shakhzod221@users.noreply.github.com>
2023-09-27 00:50:44 +0500

    My task

---
+++ main.js
@@ -0,0 +1,111 @@
+                  // String - 1
+            
+// let n = 5;
+
+// if(n >= 1 && n <= 26){
+//     let alfabit = "abcdefghijklmnopqrstuvwxyz";
+//     let firstNLetters = alfabit.slice(0, n);
+//     let newText = firstNLetters.toUpperCase()
+//     console.log(`${newText}`);
+// }else{
+//     console.log(false);
+// }
+
+
+                    // String - 2
+
+// let inp = 5;
+
+// if(typeof inp === "number"){
+//     console.log('Digit');
+// }else if(typeof inp === "string"){
+//     console.log('String');
+// }else{
+//     console.log('0');
+// }
+
+
+
+                    // String - 3
+
+// let str = "Shakhzod"
+
+// let firstChar = str.charCodeAt(0);
+// let lastChar = str.charCodeAt(str.length - 1);
+
+// console.log(`Birinchi belgini UTF - 16 kodi ${firstChar}`);
+// console.log(`Oxirgi belgini UTF - 16 kod ${lastChar}`);
+
+
+
+                    // String - 4
+
+// let n = 5;
+
+// let belgi = 'D';
+
+// let text = belgi.repeat(n);
+
+// console.log(text);
+
+
+
+                    // String - 5 
+                    
+// let str = "mano";
+
+// let newStr ="";
+
+// for(let i = str.length - 1; i >= 0; i--){
+//         newStr += str[i]
+// }
+// console.log(newStr);
+
+
+
+                  // String - 6    ????
+
+
+
+// function getStringStars(){
+
+//         let stars = "";
+
+//         for(let i = 0; i < n; i++){
+//             stars += "*";
+//         }
+//         return stars;
+// }
+
+// let satr = "Shakhzod bugun,uyga vazifani bajarmadi";
+
+// let n = 2;
+
+// let newStr = satr + getStringStars(n);
+// console.log(newStr);
+
+
+
+                  // String - 9
+
+// let str = "Shakhzod Jumaniyozov Safarboy Ogli";
+
+// let newStr = str.toLowerCase()
+
+// console.log(newStr);
+
+
+
+                  // String - 10
+
+// let str = "Shakhzod";
+
+// let newStr = str.toUpperCase();
+
+// console.log(newStr);
+
+
+
+
+
+        
